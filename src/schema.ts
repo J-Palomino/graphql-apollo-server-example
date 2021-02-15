4d1ac102b729630d8339d7d6610b85c16811f03a
Nicholas Romero <ncrmro@gmail.com>
2021-02-15 17:30:12 -0600

    feat: use reducer for dynamic schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,12 @@
 import { gql } from "apollo-server";
 import { makeExecutableSchema } from "graphql-tools";
-import Book from "./routes/books";
+import fs from 'fs'
+import { DocumentNode } from 'graphql'
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = gql`
+const typeDefsRoot = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   type Query {
@@ -19,9 +20,24 @@ const typeDefs = gql`
   }
 `;
 
-const schema = makeExecutableSchema({
-  typeDefs: [typeDefs, Book.typeDefs],
-  resolvers: [Book.resolvers],
-});
+/**
+ * For each of the folders in routes dynamically import the typeDefs and resolvers
+ * and build out the schema, this enforces the specified route folder structure
+ */
+const schema = fs.readdirSync(`${__dirname}/routes`).reduce(
+    (accumulator, item) => {
+      const {
+        typeDefs,
+        resolvers,
+      } = require(`${__dirname}/routes/${item}`).default
+      accumulator.typeDefs.push(typeDefs)
+      accumulator.resolvers.push(resolvers)
+      return accumulator
+    },
+    {
+      typeDefs: [typeDefsRoot],
+      resolvers: [],
+    } as { typeDefs: DocumentNode[]; resolvers: any[] }
+)
 
-export default schema;
+export default makeExecutableSchema(schema)
